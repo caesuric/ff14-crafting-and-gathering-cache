8dbc581e6eb742b597b968d3208e36e8fe2b7409
caesuric <illusorycaesura@gmail.com>
2024-07-12 07:28:09 -0400

    cleaned up server code

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
+"""
+Runs a web server for retrieving gathering and crafting data.
+"""
 import asyncio
 import json
 from tornado.web import Application, RequestHandler
+
+# pylint: disable=abstract-method
 
 botany_items = []
 mining_items = []
@@ -8,17 +13,26 @@ fishing_items = []
 crafting_items = {}
 
 def load_json_file(file_name, variable):
+    """
+    Loads a JSON file into the specified list.
+    """
     with open(f'data/{file_name}.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
         variable.extend(data)
 
 def load_crafting_items(file_name):
+    """
+    Loads crafting items from a JSON file.
+    """
     with open(f'data/{file_name}.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
         for k,v in data.items():
             crafting_items[k] = v
 
 async def main():
+    """
+    Main function that starts the web server.
+    """
     load_json_file('botany', botany_items)
     load_json_file('mining', mining_items)
     load_json_file('fishing', fishing_items)
@@ -38,45 +52,120 @@ async def main():
     await asyncio.Event().wait()
 
 def grab_items_for_level_range(items, min_level, max_level):
+    """
+    Grabs items within a specified level range.
+    """
     min_level = int(min_level)
     max_level = int(max_level)
     return [item for item in items if min_level <= item['level'] <= max_level]
 
 class BotanyItemsHandler(RequestHandler):
+    """
+    Request handler for botany items.
+    """
     def get(self, min_level, max_level):
+        """
+        Retrieves botany items within a specified level range.
+        """
         self.write(json.dumps(grab_items_for_level_range(botany_items, min_level, max_level)))
 
 class MiningItemsHandler(RequestHandler):
+    """
+    Request handler for mining items.
+    """
     def get(self, min_level, max_level):
+        """
+        Retrieves mining items within a specified level range.
+        """
         self.write(json.dumps(grab_items_for_level_range(mining_items, min_level, max_level)))
 
 class FishingItemsHandler(RequestHandler):
+    """
+    Request handler for fishing items.
+    """
     def get(self, min_level, max_level):
+        """
+        Retrieves fishing items within a specified level range
+        """
         self.write(json.dumps(grab_items_for_level_range(fishing_items, min_level, max_level)))
 
 class CraftingItemsHandler(RequestHandler):
+    """
+    Request handler for crafting items
+    """
     def get(self, crafting_type, min_level, max_level):
-        self.write(json.dumps(grab_items_for_level_range(crafting_items[crafting_type], min_level, max_level)))
+        """
+        Retrieves crafting items within a specified level range.
+        """
+        self.write(
+            json.dumps(
+                grab_items_for_level_range(
+                    crafting_items[crafting_type],
+                    min_level,
+                    max_level
+                )
+            )
+        )
 
 class CraftingTypesHandler(RequestHandler):
+    """
+    Request handler for crafting types.
+    """
     def get(self):
+        """
+        Retrieves crafting types.
+        """
         self.write(json.dumps(list(crafting_items.keys())))
 
 class BotanyItemsCountHandler(RequestHandler):
+    """
+    Request handler for botany item count.
+    """
     def get(self, min_level, max_level):
+        """
+        Retrieves the count of botany items within a specified level range.
+        """
         self.write(str(len(grab_items_for_level_range(botany_items, min_level, max_level))))
 
 class MiningItemsCountHandler(RequestHandler):
+    """
+    Request handler for mining item count.
+    """
     def get(self, min_level, max_level):
+        """
+        Retrieves the count of mining items within a specified level range.
+        """
         self.write(str(len(grab_items_for_level_range(mining_items, min_level, max_level))))
 
 class FishingItemsCountHandler(RequestHandler):
+    """
+    Request handler for fishing item count.
+    """
     def get(self, min_level, max_level):
+        """
+        Retrieves the count of fishing items within a specified level range.
+        """
         self.write(str(len(grab_items_for_level_range(fishing_items, min_level, max_level))))
 
 class CraftingItemsCountHandler(RequestHandler):
+    """
+    Request handler for crafting item count.
+    """
     def get(self, crafting_type, min_level, max_level):
-        self.write(str(len(grab_items_for_level_range(crafting_items[crafting_type], min_level, max_level))))
+        """
+        Retrieves the count of crafting items within a specified level range.
+        """
+        self.write(
+            str(
+                len(
+                    grab_items_for_level_range(
+                        crafting_items[crafting_type],
+                        min_level,
+                        max_level
+                    )
+                )
+            )
+        )
 
 if __name__=='__main__':
     print('Starting server.')
