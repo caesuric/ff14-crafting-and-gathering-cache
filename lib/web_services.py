7ca4a5ea3af15881223ac74facad691f87b0a8bb
caesuric <illusorycaesura@gmail.com>
2024-07-17 10:07:17 -0400

    added web endpoints for historical market data

--- lib/web_services.py
+++ lib/web_services.py
@@ -12,11 +12,12 @@ import tornado.httpserver
 from tornado.httputil import HTTPServerRequest
 from tornado.web import Application, RequestHandler
 
-from lib.universalis_data import get_current_item_data, get_tax_rates, get_worlds
+from lib.universalis_data import get_current_item_data, get_historical_item_data, get_tax_rates, get_worlds
 from lib.xivapi_data import get_basic_item_data
 
 item_jobs = {}
 current_market_jobs = {}
+historical_market_jobs = {}
 
 async def start_web_server(engine: Engine):
     """
@@ -49,6 +50,9 @@ async def start_web_server(engine: Engine):
         (r'^/rest/market-current/start/(.*)/(.*)$', MarketCurrentHandler, {'engine': engine}),
         (r'^/rest/market-current/status/(.*)$', MarketCurrentStatusHandler),
         (r'^/rest/market-current/result/(.*)$', MarketCurrentResultHandler),
+        (r'^/rest/market-historical/start/(.*)/(.*)$', MarketHistoricalHandler, {'engine': engine}),
+        (r'^/rest/market-historical/status/(.*)$', MarketHistoricalStatusHandler),
+        (r'^/rest/market-historical/result/(.*)$', MarketHistoricalResultHandler),
     ])
     http_server = tornado.httpserver.HTTPServer(
         application,
@@ -248,6 +252,90 @@ class MarketCurrentResultHandler(BaseHandler):
         self.write(json.dumps(status['items']))
         if status['complete']:
             del current_market_jobs[job_id]
+
+
+class MarketHistoricalHandler(BaseHandler):
+    """
+    Request handler for historical market data.
+    """
+
+    def __init__(self, application: Application, request: HTTPServerRequest, **kwargs):
+        super().__init__(application, request, **kwargs)
+        self.engine: Optional[Engine] = None
+        self.status: Optional[dict] = None
+        self.job_id: str = str(uuid4())
+        if 'engine' in kwargs:
+            self.engine = kwargs['engine']
+
+    def initialize(self, **kwargs):
+        """
+        Initializes the handler.
+        """
+        self.engine = kwargs['engine']
+
+    def get(self, world: Optional[str], raw_item_ids: Optional[str]):
+        """
+        Starts a job to retrieve a list of historical market data, and returns a job ID.
+        """
+        if self.engine is None:
+            self.set_status(500, 'Database engine not passed to handler.')
+            return
+        if ',' not in raw_item_ids:
+            item_ids = [int(raw_item_ids)]
+        else:
+            item_ids = raw_item_ids.split(',')
+        item_ids = [int(item_id) for item_id in item_ids]
+        self.write(self.job_id)
+        process = Thread(target=self.get_async_portion, args=(world, item_ids))
+        process.start()
+
+    def get_async_portion(self, world: str, item_ids: list[int]):
+        """
+        Asynchronous portion of the get request.
+
+        Args:
+            world (str): World for which to retrieve market data.
+            item_ids (list[int]): IDs of the items for which to retrieve market data.
+        """
+        for state in get_historical_item_data(world, item_ids, self.engine):
+            self.status = state
+            historical_market_jobs[self.job_id] = state
+
+
+class MarketHistoricalStatusHandler(BaseHandler):
+    """
+    Request handler for current market job status.
+    """
+
+    def get(self, job_id: str):
+        """
+        Retrieves the status of an item job.
+        """
+        if job_id not in historical_market_jobs:
+            self.set_status(404, 'Job ID not found.')
+            self.write(json.dumps({}))
+            return
+        status = historical_market_jobs[job_id].copy()
+        status['items'] = {}
+        self.write(json.dumps(status))
+
+
+class MarketHistoricalResultHandler(BaseHandler):
+    """
+    Request handler for current market job results.
+    """
+
+    def get(self, job_id: str):
+        """
+        Retrieves the results of an item job.
+        """
+        if job_id not in historical_market_jobs:
+            self.set_status(404, 'Job ID not found.')
+            return
+        status = historical_market_jobs[job_id].copy()
+        self.write(json.dumps(status['items']))
+        if status['complete']:
+            del historical_market_jobs[job_id]
 
 
 class WorldsHandler(BaseHandler):
